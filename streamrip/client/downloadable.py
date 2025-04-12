117ca9564e9ba3eb6178bd021b4b88ece187c378
slanglade <1507663+slanglade@users.noreply.github.com>
2025-04-12 17:39:02 +0200

    Fix for Rufus

--- streamrip/client/downloadable.py
+++ streamrip/client/downloadable.py
@@ -47,8 +47,8 @@ async def fast_async_download(path, url, headers, callback):
     chunk_size: int = 2**17  # 131 KB
     counter = 0
     yield_every = 8  # 1 MB
-    with open(path, "wb") as file:  # noqa: ASYNC101, ASYNC230
-        with requests.get(  # noqa: ASYNC100, ASYNC230
+    with open(path, "wb") as file:  # noqa: ASYNC101, ASYNC210
+        with requests.get(  # noqa: ASYNC100, ASYNC210
             url,
             headers=headers,
             allow_redirects=True,
--- streamrip/media/track.py
+++ streamrip/media/track.py
@@ -90,7 +90,7 @@ class Track(Media):
             self.db.set_downloaded(self.meta.info.id, self.download_path)
             if self.m3u8:
                 try:
-                    with open(self.m3u8, 'a+') as f: # noqa: ASYNC230
+                    with open(self.m3u8, 'a+') as f: # noqa: ASYNC101, ASYNC210
                         # Write filepath using relative path. Given m3u8 file is located in the same folder structure, simply replace its path with "."
                         f.write(self.download_path.replace(os.path.dirname(self.m3u8), ".") + "\n")
                 except Exception as e:
